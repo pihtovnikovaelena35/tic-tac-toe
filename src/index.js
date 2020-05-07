f2d426792ba562722010b61fa9a184ed53e59a9f
Elena Pihtovnikova <pihtovnikovaelena35@gmail.com>
2020-05-07 12:39:26 +0300

    priority 'X' and 'O'

--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-class Square extends React.Component {
-  render() {
+function Square (props) {
     return (
       <button 
       className="square" 
-      onClick={() => this.props.onClick()}
+      onClick={props.onClick}
       >
-        {this.props.value}
+        {props.value}
       </button>
     );
   }
-}
+
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext:true,
     };
   }
     handleClick(i) {
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
   
   renderSquare(i) {
@@ -36,7 +39,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Следующий ход: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
