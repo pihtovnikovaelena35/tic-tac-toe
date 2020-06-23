c8b9473965a1864451e890128ec48bc7e8d5a313
Elena Pihtovnikova <pihtovnikovaelena35@gmail.com>
2020-06-23 19:26:39 +0300

    Updates

---
+++ precache-manifest.85ed3ec6b12a81781bdb78365deb26a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db374b41fbc9d0e8d9aec3f0b220d11",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "0bd4a735ff8f2dd04548",
+    "url": "/tic-tac-toe/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "38c58a404a3fb46dd5f4",
+    "url": "/tic-tac-toe/static/js/2.b102e267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.b102e267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bd4a735ff8f2dd04548",
+    "url": "/tic-tac-toe/static/js/main.afb7ff8b.chunk.js"
+  },
+  {
+    "revision": "2e3e4e44d6a5c7256d12",
+    "url": "/tic-tac-toe/static/js/runtime-main.f4158612.js"
+  }
+]);
